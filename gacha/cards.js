0eb49dfbfc0bc7d1f9ebdfa20dec44351f8d91b3
allheroesband-create <allheroesband@gmail.com>
2026-01-08 12:06:01 +0900

    Update cards.js

--- gacha/cards.js
+++ gacha/cards.js
@@ -1,7 +1,50 @@
 // ========================================
-// カードデータのインポート
+// カードデータの定義
 // ========================================
-import { CARDS } from './gacha/assets';
+export const CARDS = [
+  {
+    id: "diddy",
+    name: "DIDDY",
+    rarity: "Special",
+    image: "./assets/DIDDY.png"
+  },
+  {
+    id: "elon_musk",
+    name: "ELON MUSK",
+    rarity: "Colonel",
+    image: "./assets/ELON_MUSK2.png"
+  },
+  {
+    id: "right_hand",
+    name: "RIGHT HAND",
+    rarity: "Colonel",
+    image: "./assets/RIGHT_HAND.png"
+  },
+  {
+    id: "mark_zuckerberg",
+    name: "MARK ZUCKERBERG",
+    rarity: "Lieutenant Colonel",
+    image: "./assets/Mark_Zuckerberg.png"
+  },
+  {
+    id: "power_of_friendship",
+    name: "THE POWER OF FRIENDSHIP",
+    rarity: "Colonel",
+    image: "./assets/The_Power_of_Friendship.png"
+  },
+  {
+    id: "epstein",
+    name: "JEFFREY EPSTEIN",
+    rarity: "Special",
+    image: "./assets/Epstein.png"
+  },
+  {
+    id: "peter_thiel",
+    name: "PETER THIEL",
+    rarity: "Colonel",
+    image: "./assets/Peter_Thiel2.png"
+  }
+];
 
 // ========================================
 // レアリティとホロエフェクトのマッピング
@@ -503,4 +546,5 @@ export {
   displayCardsByRarity,
   CardCollection
 };
+
 
